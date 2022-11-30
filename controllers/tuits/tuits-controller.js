1717c455a40054a1a52cd771202927a202f80502
Lingfei Zhang <lingfeiz@Lingfeis-MacBook-Pro.local>
2022-11-30 11:53:38 -0800

    a9 Mongo fa22

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,34 +1,46 @@
-import posts from "./tuits.js";
-let tuits = posts;
+// import posts from "./tuits.js";
+// let tuits = posts;
+import * as tuitsDao from './tuits-dao.js'
+import mongoose from "mongoose"
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit._id = (new Date()).getTime() + '';
+  // newTuit._id = (new Date()).getTime() + '';
   newTuit.likes = 0;
   newTuit.liked = false;
   newTuit.dislikes=0;
-  tuits.push(newTuit);
-  res.json(newTuit);
+  // tuits.push(newTuit);
+  const insertedTuit = await tuitsDao
+  .createTuit(newTuit);
+  res.json(insertedTuit);
 }
 
-const findTuits = (req, res) =>
-    res.json(tuits);
-const updateTuit = (req, res) => {
-  const tuitdIdToUpdate = req.params.tid;
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits()
+  res.json(tuits);
+}
+
+const updateTuit = async (req, res) => {
+  const tuitdIdToUpdate = mongoose.Types.ObjectId(req.params.tid);
   const updates = req.body;
-  const tuitIndex = tuits.findIndex(
-      (t) => t._id === tuitdIdToUpdate)
-  tuits[tuitIndex] =
-      {...tuits[tuitIndex], ...updates};
-  res.sendStatus(200);
+  // const tuitIndex = tuits.findIndex(
+  //     (t) => t._id === tuitdIdToUpdate)
+  // tuits[tuitIndex] =
+  //     {...tuits[tuitIndex], ...updates};
+  const status = await tuitsDao
+  .updateTuit(tuitdIdToUpdate,
+      updates);
+  res.json(status);
 }
 
 
-const deleteTuit = (req, res) => {
-  const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) =>
-      t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
+const deleteTuit = async (req, res) => {
+  const tuitdIdToDelete = mongoose.Types.ObjectId(req.params.tid);
+  // tuits = tuits.filter((t) =>
+  //     t._id !== tuitdIdToDelete);
+  const status = await tuitsDao
+  .deleteTuit(tuitdIdToDelete);
+  res.json(status);
 }
 
 
